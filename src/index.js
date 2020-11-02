662b98d97fc116311206100f7755bca67d55e553
stephbui <stephanie.bui@umontreal.ca>
2020-11-02 12:08:54 -0500

    added relevant weather icon

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ function displayWeatherCondition(response) {
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-
   celsiusTemperature = response.data.main.temp;
-  console.log(response)
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
-
 
 function formatDate(date) {
   let hours = date.getHours();
@@ -82,11 +83,6 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 
-
-
-
-
-
 function searchLocation(position) {
   let apiKey = "a68b01f6acebcedd5ae9fe5ee2fca1da";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
@@ -100,16 +96,11 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-
-
 function converttoCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = 19;
 }
-
-
-
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
