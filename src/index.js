1b885cee0e4fd19770b0fc2f6f62284cd09b2ed1
stephbui <stephanie.bui@umontreal.ca>
2020-11-03 19:29:59 -0500

    css background

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function displayForecast(response) {
   forecastElement.innerHTML = null;
   let forecast = null;
 
-  for (let index = 0; index < 5; index++) {
+  for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
     <div class="col-4">
